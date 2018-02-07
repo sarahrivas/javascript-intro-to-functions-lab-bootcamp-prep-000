6007aa6addd393edd39162323fd53fa00c08b683
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-07 03:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function shout (string) {
   return string.toUpperCase();
 }
+
+function whisper (string) {
+  return string.toLowerCase();
+}
+
+function logShout (string) {
+  console.log(string.ToUpperCase());
+}
+
+function logWhisper (string) {
+  console.log(string.ToLowerCase());
+}
+
+function sayHiToGrandma (string) {
+  if(string === "I love you, Grandma.") {
+    return "I love you too";
+  } else if(string === string.toUpperCase()) {
+    return "YES INDEED!";
+  } else if(string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+}
